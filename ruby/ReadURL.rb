96d4de41e6f4c9f6749d67b59d0301ed1e123eaa
fionnmaccumhaill <edward.foley@comcast.net>
2016-01-02 20:11:41 -0500

    add ToDO note to ReadURL.rb

--- ruby/ReadURL.rb
+++ ruby/ReadURL.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+# add some quick tests and css selector functionality
+# to show how a page is parsed
 def getPage(aURL)
     return Nokogiri::HTML(open(aURL))  
 end
